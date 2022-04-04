45ec30128c7f738495c4394559de76cd0d78ebf7
Joris Wiethase <j.wiethase@gmail.com>
2022-04-04 10:49:20 +0100

    misc edits

--- Scripts/Make_data.R
+++ Scripts/Make_data.R
@@ -7,7 +7,7 @@ library(raster)
 library(rgdal)
 
 # setwd('/Users/philism/OneDrive - NTNU/PhD/Joris_work/Scripts')
-setwd('/Users/joriswiethase/Google Drive (jhw538@york.ac.uk)/Work/PhD_York/Chapter3/TZ_INLA/source')
+setwd('/Users/joriswiethase/Google Drive (jhw538@york.ac.uk)/Work/PhD_York/Chapter3/TZ_inla_spatial_temporal/source')
 sapply(list.files(pattern="*.R"),source,.GlobalEnv)
 
 # Import and prepare data-----------------------------------------------------------------------------------
@@ -61,15 +61,14 @@ TZ_dryspell_80_00 <- raster('TZbuff_median_annual_dryspell_length_1981_1999.tif'
 TZ_dryspell_80_00[is.nan(TZ_dryspell_80_00)] <- NA
 TZ_dryspell_00_20 <- raster('TZbuff_median_annual_dryspell_length_2000_2020.tif') %>% mask(., ROI) %>% projectRaster(., TZ_annual_median_rain_80_00)
 TZ_dryspell_00_20[is.nan(TZ_dryspell_00_20)] <- NA
-TZ_BG_90_99 <- raster('BG_1990_1999_1000m.tif') %>% mask(., ROI) %>% projectRaster(., TZ_annual_median_rain_80_00)
-TZ_BG_90_99[is.nan(TZ_BG_90_99)] <- NA
-TZ_BG_10_19 <- raster('BG_2010_19_1000m.tif') %>% mask(., ROI) %>% projectRaster(., TZ_annual_median_rain_80_00)
-TZ_BG_10_19[is.nan(TZ_BG_10_19)] <- NA
+# TZ_BG_90_99 <- raster('BG_1990_1999_1000m.tif') %>% mask(., ROI) %>% projectRaster(., TZ_annual_median_rain_80_00)
+# TZ_BG_90_99[is.nan(TZ_BG_90_99)] <- NA
+# TZ_BG_10_19 <- raster('BG_2010_19_1000m.tif') %>% mask(., ROI) %>% projectRaster(., TZ_annual_median_rain_80_00)
+# TZ_BG_10_19[is.nan(TZ_BG_10_19)] <- NA
 
 temporal_variables <- stack(TZ_annual_median_rain_80_00, TZ_annual_median_rain_00_20, 
                             TZ_ERA5_hottest_80_00, TZ_ERA5_hottest_00_20,
-                            TZ_dryspell_80_00, TZ_dryspell_00_20,
-                            TZ_BG_90_99, TZ_BG_10_19)
+                            TZ_dryspell_80_00, TZ_dryspell_00_20)
 
 names(temporal_variables) <- c('TZ_ann_rain_1980s', 'TZ_ann_rain_2000s', 
                                'TZ_max_temp_1980s', 'TZ_max_temp_2000s',
@@ -80,35 +79,14 @@ temporal_variables <- temporal_variables[!is.na(rowSums(temporal_variables@data)
 
 # Prepare data for a GAM model, keep simple to avoid overfitting (two spline bases, limited flexibility GAM). 
 # Separates out and scales a prediction vector
+
 for (i in 1:length(names(temporal_variables))){
       temporal_variables <- prepare_GAM(temporal_variables, names(temporal_variables)[i])
       assign("temporal_variables", temporal_variables, envir = .GlobalEnv)
 }
 
-prepare_GAM <- function(df, colname){
-      # Create prediction vector, as sequence of 100 values, spanning full range of values. 
-      seq <- seq(min(df@data[, colname], na.rm = TRUE), max(df@data[, colname], na.rm = TRUE), length = 100)
-      # Add prediction vector to beginning of original values, scale all to mean 0 and sd 1
-      var.s <- c(scale(c(seq, df@data[, colname])))  
-      # Split the full vector into two spline bases, and scale the new vectors to mean 0 and sd 1.
-      # Explain:
-      z.var.s <- scale(sortBase(var.s, n.knots = 2))  
-      
-      # Add the two scaled spline bases back to the original dataset. Omit the first 100 values,
-      # this is the prediction vector, not original data
-      df@data[, paste0("z.", colname, "1.s")] <- c(z.var.s[101:NROW(z.var.s), 1])
-      df@data[, paste0("z.", colname, "2.s")] <- c(z.var.s[101:NROW(z.var.s), 2])
-      
-      # Add original, but scaled values back to the dataset
-      df@data[, paste0(colname, ".s")]  <- var.s[-c(1:100)]
-      
-      ## Separate out prediction vectors for the two spline bases, and assign to environment as
-      ## separate objects
-      assign(paste0("z.", colname, "1.s"), c(z.var.s[1:100, 1]), envir = .GlobalEnv)
-      assign(paste0("z.", colname, "2.s"), c(z.var.s[1:100, 2]), envir = .GlobalEnv)
-      
-      return(df)
-}
+# Combine the unscaled prediction vectors
+all.seq <- mget(ls(pattern = "*s.seq"))
 
 # Make linear combinations, needed to make effect plots
 TZ_max_temp_1980s_lc <- inla.make.lincombs(ebird_intercept = rep(1, 100),  
@@ -218,4 +196,4 @@ stk.pred <- MakeProjectionGrid(
 # setwd('/Users/philism/OneDrive - NTNU/PhD/Joris_work/Philip_data')
 setwd('/Users/joriswiethase/Google Drive (jhw538@york.ac.uk)/Work/PhD_York/Chapter3/TZ_INLA/data_processed')
 
-save(proj, ROI, ebird_full, atlas_full, Mesh, stk.ip, stk.pred, temporal_variables, TZ_outline, all_lc, file = paste0("TZ_INLA_model_file_temporal_E", round(max.edge, digits = 3), ".RData"))
+save(proj, ROI, ebird_full, atlas_full, Mesh, stk.ip, stk.pred, temporal_variables, TZ_outline, all_lc, all.seq, file = paste0("TZ_INLA_model_file_temporal_E", round(max.edge, digits = 3), ".RData"))
