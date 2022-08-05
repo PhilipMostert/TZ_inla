b31c51a2779c53d200348495cb0bcba750ebcc76
Joris Wiethase <j.wiethase@gmail.com>
2022-08-05 08:47:46 +0100

    Add functions for results figures

--- source/misc_functions.R
+++ source/misc_functions.R
@@ -179,5 +179,155 @@ cloglog_inv <- function(x){
       1-exp(-exp(x))
 }
 
+# make_lincomb_contrasts <- function(data, colname){
+#       k <- nlevels(data[, colname])
+#       if(k < 3 | k > 4){
+#             stop("Currently only works for factors with 3 or 4 levels.")
+#       }
+#       if (k == 4) {
+#             ncombs = 6
+#             LC <- inla.make.lincombs(lc1 = c( 1,  1,  1,  0,  0, 0),
+#                                      lc2 = c(-1,  0,  0,  1,  1, 0),
+#                                      lc3 = c( 0, -1,  0, -1,  0, 1),
+#                                      lc4 = c( 0,  0, -1,  0, -1,-1))
+#       }
+#       
+#       if (k == 3) {
+#             ncombs = 3
+#             LC <- inla.make.lincombs(lc1 = c( 1,  1,  0),
+#                                      lc2 = c(-1,  0, -1),
+#                                      lc3 = c( 0, -1,  1))
+#       }
+# 
+#       for(i in 1:ncombs){
+#             for (j in 1:k){
+#                   names(LC[[paste0("lc", i)]][[j]]) <- paste0(colname, levels(data[, colname])[j])
+#             }
+#       }
+#       
+#       # Assign names to combinations
+#       t <- 0
+#       for(i in 1:(k-1)) {
+#             for(j in (i+1):k) {
+#                   tryCatch({
+#                         t <- t + 1
+#                         names(LC)[t] <- paste0(levels(data[, colname])[i], "-", levels(data[, colname])[j])
+#                   }, error=function(e){})
+#             }
+#       }
+#       return(LC)
+# }
+# 
+# 
+# 
+lincomb_output <- function(model, data){
+      lincomb_out_df <- data.frame(model$summary.lincomb.derived) %>% 
+            mutate(ID = gsub('[[:digit:]]+', '', rownames(.))) %>% 
+            dplyr::select(-mode, -kld, -mean, -sd) %>% 
+            pivot_wider(names_from = ID, values_from = c(2:4),
+                        names_glue = "{ID}_{.value}") %>% 
+            unnest()
+      
+      lincomb_out_df[, strsplit(colnames(lincomb_out_df)[grep("BG", colnames(lincomb_out_df))], split = "_X")[[1]][1]] <- 
+            seq(min(data[, strsplit(colnames(lincomb_out_df)[grep("BG", colnames(lincomb_out_df))], split = "_X")[[1]][1]]),
+                max(data[, strsplit(colnames(lincomb_out_df)[grep("BG", colnames(lincomb_out_df))], split = "_X")[[1]][1]]),
+                len = 100)
+      lincomb_out_df[, strsplit(colnames(lincomb_out_df)[grep("rain", colnames(lincomb_out_df))], split = "_X")[[1]][1]] <- 
+            seq(min(data[, strsplit(colnames(lincomb_out_df)[grep("rain", colnames(lincomb_out_df))], split = "_X")[[1]][1]]),
+                max(data[, strsplit(colnames(lincomb_out_df)[grep("rain", colnames(lincomb_out_df))], split = "_X")[[1]][1]]),
+                len = 100)
+      lincomb_out_df[, strsplit(colnames(lincomb_out_df)[grep("temp", colnames(lincomb_out_df))], split = "_X")[[1]][1]] <- 
+            seq(min(data[, strsplit(colnames(lincomb_out_df)[grep("temp", colnames(lincomb_out_df))], split = "_X")[[1]][1]]),
+                max(data[, strsplit(colnames(lincomb_out_df)[grep("temp", colnames(lincomb_out_df))], split = "_X")[[1]][1]]),
+                len = 100)
+      lincomb_out_df[, strsplit(colnames(lincomb_out_df)[grep("dry", colnames(lincomb_out_df))], split = "_X")[[1]][1]] <- 
+            seq(min(data[, strsplit(colnames(lincomb_out_df)[grep("dry", colnames(lincomb_out_df))], split = "_X")[[1]][1]]),
+                max(data[, strsplit(colnames(lincomb_out_df)[grep("dry", colnames(lincomb_out_df))], split = "_X")[[1]][1]]),
+                len = 100)
+      lincomb_out_df[, strsplit(colnames(lincomb_out_df)[grep("HFP", colnames(lincomb_out_df))], split = "_X")[[1]][1]] <- 
+            seq(min(data[, strsplit(colnames(lincomb_out_df)[grep("HFP", colnames(lincomb_out_df))], split = "_X")[[1]][1]]),
+                max(data[, strsplit(colnames(lincomb_out_df)[grep("HFP", colnames(lincomb_out_df))], split = "_X")[[1]][1]]),
+                len = 100)
+      lincomb_out_df[, strsplit(colnames(lincomb_out_df)[grep("Wing.Length", colnames(lincomb_out_df))], split = "_X")[[1]][1]] <- 
+            seq(min(data[, strsplit(colnames(lincomb_out_df)[grep("Wing.Length", colnames(lincomb_out_df))], split = "_X")[[1]][1]]),
+                max(data[, strsplit(colnames(lincomb_out_df)[grep("Wing.Length", colnames(lincomb_out_df))], split = "_X")[[1]][1]]),
+                len = 100)
+      lincomb_out_df[, strsplit(colnames(lincomb_out_df)[grep("avg.r", colnames(lincomb_out_df))], split = "_X")[[1]][1]] <- 
+            seq(min(data[, strsplit(colnames(lincomb_out_df)[grep("avg.r", colnames(lincomb_out_df))], split = "_X")[[1]][1]], na.rm = TRUE),
+                max(data[, strsplit(colnames(lincomb_out_df)[grep("avg.r", colnames(lincomb_out_df))], split = "_X")[[1]][1]], na.rm = TRUE),
+                len = 100)
+      
+      return(lincomb_out_df)
+}
 
+make_forest_plot <- function(inla_model, label, col){
+      model_fixed <- data.frame(inla_model$summary.fixed) %>% 
+            mutate(ID = c("Intercept", "Trophic level: Carnivore", "Trophic level: Herbivore", 
+                          "Primary lifestyle: Generalist", "Primary lifestyle: Insessorial", "Primary lifestyle: Terrestrial",
+                          "Migratory ability: Moderate", "Migratory ability: High",
+                          "Bare ground cover", "Annual rainfall", "Hottest temperature", "Dryspell duration",
+                          "Human footprint", "Wing length", "Dorsal reflectance")
+            )
+      model_fixed$ID <- factor(model_fixed$ID, levels = c("Trophic level: Carnivore", "Trophic level: Herbivore", 
+                                                          "Primary lifestyle: Generalist", "Primary lifestyle: Insessorial", "Primary lifestyle: Terrestrial",
+                                                          "Migratory ability: Moderate", "Migratory ability: High", "Wing length", "Dorsal reflectance",
+                                                          "Bare ground cover", "Annual rainfall", "Hottest temperature", "Dryspell duration", "Human footprint",
+                                                          "Intercept"))
+      model_fixed$significant <- ifelse((model_fixed$X0.025quant > 0 & model_fixed$X0.975quant > 0)|(model_fixed$X0.025quant < 0 & model_fixed$X0.975quant < 0), "yes", "no")
+      
+      forest_plot <- ggplot() + 
+            annotate("text", x = 9.8, y = max(inla_model$summary.fixed$`0.975quant`), col = col, label = label) +
+            geom_rect(aes(xmin = 1.5, xmax = 6.5, ymin = min(inla_model$summary.fixed$`0.025quant`), 
+                          ymax = max(inla_model$summary.fixed$`0.975quant`)),
+                      fill = col, col = col, alpha = .3) +
+            geom_point(data = model_fixed, aes(x = ID, y = X0.5quant, col = significant)) +
+            geom_errorbar(data = model_fixed, aes(x = ID, ymin = X0.025quant, ymax = X0.975quant, col = significant), width = 0.1) +
+            geom_hline(aes(yintercept = 0), lty = 2, alpha = .3) +
+            # geom_text(data = model_fixed %>% filter(significant == "yes"), aes(x = ID, y = X0.975quant), label = "*", nudge_x = 0.1, nudge_y = 0.01) +
+            ylab("Posterior estimates") +
+            xlab(element_blank()) +
+            theme_minimal() +
+            scale_x_discrete(limits=rev) +
+            scale_colour_manual(values = c("black", "#D55E00"), guide = "none") +
+            theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+      return(forest_plot)
+}
 
+rsq <- function(x, y) summary(lm(y~x))$r.squared
+
+make_raw_data_plots <- function(inla_model, data, xlabel){
+      covariates <- strsplit(as.character(inla_model$.args$formula[3]), split = " \\+ ")[[1]]
+      response <- as.character(inla_model$.args$formula[2])
+      plot_list <- vector('list', length(covariates))
+      
+      lincomb_data <- lincomb_output(inla_model, data)
+      i <- 4
+      for (i in 1:length(covariates)){
+            if(is.numeric(data[, covariates[i]])){
+                  plot_list[[i]] <- local({
+                        i <- i
+                        p1 <- ggplot() +
+                              geom_point(data = data, aes(x = get(covariates[i]), y = get(response))) +
+                              geom_line(data = lincomb_data, aes(x = get(covariates[i]), y =  get(paste0(covariates[i], "_X0.5quant"))), lty = 2, alpha = 0.5) +
+                              geom_ribbon(data = lincomb_data, aes(x = get(covariates[i]), 
+                                                                   ymin = get(paste0(covariates[i], "_X0.025quant")), 
+                                                                   ymax = get(paste0(covariates[i], "_X0.975quant"))), alpha = 0.2) +
+                              theme_classic() +
+                              xlab(covariates[i]) +
+                              ylab(xlabel) + 
+                              geom_rug(data = data, aes(x = get(covariates[i]), y = get(response)), col=rgb(.5,0,0, alpha=.4))
+                        print(p1)})
+            } else {
+                  plot_list[[i]] <- local({
+                        i <- i
+                        p2 <- ggplot(data) +
+                              geom_boxplot(aes(get(covariates[i]), get(response))) +
+                              theme_classic() +
+                              xlab(covariates[i]) +
+                              ylab(xlabel)
+                        print(p2)})
+            }
+      }
+      
+      return(plot_list)
+}
