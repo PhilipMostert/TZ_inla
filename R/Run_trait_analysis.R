484d4eb5dcbe23d7f38be91e80904b6e07b4cf05
Joris Wiethase <j.wiethase@gmail.com>
2023-05-01 11:03:52 -0400

    remove unnecessary libraries

--- R/Run_trait_analysis.R
+++ R/Run_trait_analysis.R
@@ -1,19 +1,8 @@
 rm(list = ls())
-library(INLA)
-library(INLAutils)
-library(brinla)
 library(tidyverse)
-library(GGally)
-library(raster)
-library(readxl)
-library(ggpubr)
-library(patchwork)
-library(ggthemes)
-library(ztable)
-library(magrittr)
+library(INLA)
 
-
-# Needs R 4.1.2, INLA 22.04.16, for robust regression
+# Needs R 4.1.2, INLA 22.04.16 for robust regression
 
 # -----------------------------------------------------------------------------------------------------------------
 # Data import
@@ -35,10 +24,6 @@ mean(model_data$auc_mean)
 min(model_data$auc_mean)
 max(model_data$auc_mean)
 
-# model_data <- model_data %>% 
-#       filter(species != "Pseudonigrita cabanisi", 
-#              !is.na(avg.r))
-
 # -----------------------------------------------------------------------------------------------------------------
 # Statistical test
 # -----------------------------------------------------------------------------------------------------------------
@@ -50,75 +35,73 @@ model_prop_change <- inla(form_prop_change, data = model_data,
                lincomb = all_lc_sens,
                control.compute = list(dic = TRUE, cpo = TRUE),
                control.predictor=list(compute=TRUE))
-# brinla::bri.lmresid.plot(model_prop_change)  
+
 hist(model_prop_change$cpo$pit, main="", breaks = 10, xlab = "PIT")
 qqplot(qunif(ppoints(length(model_prop_change$cpo$pit))),
        model_prop_change$cpo$pit, main = "Q-Q plot for Unif(0,1)", xlab = "Theoretical Quantiles", ylab = "Sample Quantiles")
 qqline(model_prop_change$cpo$pit, distribution = function(p) qunif(p), prob = c(0.1, 0.9))
 
-
 form_col_log <- log_relative_colonisation ~ Trophic.Level + Primary.Lifestyle + Migratory_ability + 
       BG_imp + rain_imp + temp_imp + dry_imp + HFP_imp + HWI + Mass + avg.r
+
 model_col_log <- inla(form_col_log, data = model_data, 
                          family = "T",  
                          lincomb = all_lc_sens,
                          control.compute = list(dic = TRUE, cpo = TRUE, return.marginals.predictor=TRUE), 
                          control.predictor=list(compute=TRUE))
 
-# brinla::bri.lmresid.plot(model_col_log)  
 hist(model_col_log$cpo$pit, main="", breaks = 10, xlab = "PIT")
 qqplot(qunif(ppoints(length(model_col_log$cpo$pit))), model_col_log$cpo$pit, main = "Q-Q plot for Unif(0,1)", xlab = "Theoretical Quantiles", ylab = "Sample Quantiles")
 qqline(model_col_log$cpo$pit, distribution = function(p) qunif(p), prob = c(0.1, 0.9))
 
 form_loss_log <- log_relative_extinction ~ Trophic.Level + Primary.Lifestyle + Migratory_ability + 
       BG_imp + rain_imp + temp_imp + dry_imp + HFP_imp + HWI + Mass + avg.r
+
 model_loss_log <- inla(form_loss_log, data = model_data, 
                               family = "T", 
                               lincomb = all_lc_sens,
                               control.compute = list(dic = TRUE, cpo = TRUE, return.marginals.predictor=TRUE), 
                               control.predictor=list(compute=TRUE))
 
-# brinla::bri.lmresid.plot(model_loss_log)  
 hist(model_loss_log$cpo$pit, main="", breaks = 10, xlab = "PIT")
-title("Log normalized extinction probability")
 qqplot(qunif(ppoints(length(model_loss_log$cpo$pit))), model_loss_log$cpo$pit, main = "Q-Q plot for Unif(0,1)", xlab = "Theoretical Quantiles", ylab = "Sample Quantiles")
 qqline(model_loss_log$cpo$pit, distribution = function(p) qunif(p), prob = c(0.1, 0.9))
 
 form_prop_change_breadth <- log_prop_change ~ Trophic.Level + Primary.Lifestyle + Migratory_ability + 
       rain_breadth + temp_breadth + dry_breadth + HFP_breadth + BG_breadth + HWI + Mass + avg.r
+
 model_prop_change_breadth <- inla(form_prop_change_breadth, data = model_data, 
                                  family = "T", 
                                  lincomb = all_lc_breadth,
                                  control.compute = list(dic = TRUE, cpo = TRUE, return.marginals.predictor=TRUE), 
                                  control.predictor=list(compute=TRUE))
 
-# brinla::bri.lmresid.plot(model_prop_change_breadth)  
 hist(model_prop_change_breadth$cpo$pit, main="", breaks = 10, xlab = "PIT")
 qqplot(qunif(ppoints(length(model_prop_change_breadth$cpo$pit))), model_prop_change_breadth$cpo$pit, main = "Q-Q plot for Unif(0,1)", xlab = "Theoretical Quantiles", ylab = "Sample Quantiles")
 qqline(model_prop_change_breadth$cpo$pit, distribution = function(p) qunif(p), prob = c(0.1, 0.9))
 
 form_col_log_breadth <- log_relative_colonisation ~ Trophic.Level + Primary.Lifestyle + Migratory_ability + 
       rain_breadth + temp_breadth + dry_breadth + HFP_breadth + BG_breadth + HWI + Mass + avg.r
+
 model_col_log_breadth <- inla(form_col_log_breadth, data = model_data, 
                                  family = "T",  
                                  lincomb = all_lc_breadth,
                                  control.compute = list(dic = TRUE, cpo = TRUE, return.marginals.predictor=TRUE), 
                                  control.predictor=list(compute=TRUE))
 
-# brinla::bri.lmresid.plot(model_col_log_breadth)  
 hist(model_col_log_breadth$cpo$pit, main="", breaks = 10, xlab = "PIT")
 qqplot(qunif(ppoints(length(model_col_log_breadth$cpo$pit))), model_col_log_breadth$cpo$pit, main = "Q-Q plot for Unif(0,1)", xlab = "Theoretical Quantiles", ylab = "Sample Quantiles")
 qqline(model_col_log_breadth$cpo$pit, distribution = function(p) qunif(p), prob = c(0.1, 0.9))
 
 form_loss_log_breadth <- log_relative_extinction ~ Trophic.Level + Primary.Lifestyle + Migratory_ability + 
       rain_breadth + temp_breadth + dry_breadth + HFP_breadth + BG_breadth + HWI + Mass + avg.r
+
 model_loss_log_breadth <- inla(form_loss_log_breadth, data = model_data, 
                                       family = "T",  
                                       lincomb = all_lc_breadth,
                                       control.compute = list(dic = TRUE, cpo = TRUE, return.marginals.predictor=TRUE), 
                                       control.predictor=list(compute=TRUE))
 
-# brinla::bri.lmresid.plot(model_loss_log_breadth)  
 hist(model_loss_log_breadth$cpo$pit, main="", breaks = 10, xlab = "PIT")
 qqplot(qunif(ppoints(length(model_loss_log_breadth$cpo$pit))), model_loss_log_breadth$cpo$pit, main = "Q-Q plot for Unif(0,1)", xlab = "Theoretical Quantiles", ylab = "Sample Quantiles")
 qqline(model_loss_log_breadth$cpo$pit, distribution = function(p) qunif(p), prob = c(0.1, 0.9))
